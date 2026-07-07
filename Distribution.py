017d9a972c0094af487dab0cdf4abd8a211bb8ac
kbaridon <kilfen@outlook.fr>
2026-07-07 10:36:27 +0200

    fix: colors and tight layout

--- Distribution.py
+++ Distribution.py
@@ -34,23 +34,27 @@ def main():
                     img.verify()
                 data_dict[d] += 1
 
-        bar_colors = ["tab:red", "tab:blue", "tab:green", "tab:orange"]
+        labels = list(data_dict.keys())
+        values = list(data_dict.values())
+        cmap = plt.get_cmap("tab20")
+        bar_colors = [cmap(i % cmap.N) for i in range(len(labels))]
+
+        plt.figure(figsize=(10, 10))
 
         plt.subplot(2, 1, 1)
-        plt.bar(data_dict.keys(), data_dict.values(), color=bar_colors)
-
-        data_values = list(data_dict.values())
-        data_sum = 0
-        for i in data_values:
-            data_sum += i
-
-        pie_labels = []
-        for i in data_values:
-            pie_labels.append(f"{round((i / data_sum) * 100, 1)}%")
+        plt.bar(labels, values, color=bar_colors)
 
         plt.subplot(2, 1, 2)
-        plt.pie(data_dict.values(), autopct="%1.1f%%", colors=bar_colors)
+        wedges, _, _ = plt.pie(values, autopct="%1.1f%%", colors=bar_colors)
+        plt.legend(
+            wedges,
+            labels,
+            loc="center left",
+            bbox_to_anchor=(1, 0.5),
+            fontsize="small",
+        )
         plt.title("class distribution")
+        plt.tight_layout()
         plt.show()
 
     except Exception as e:
